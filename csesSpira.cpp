711d3778493cd1103c0480f4a7f99bc4ca5dc168
Arpit Gupta <theburningdesiresince2002@gmail.com>
2021-07-21 21:00:40 +0530

    cses Spiral modified

--- csesSpira.cpp
+++ csesSpira.cpp
@@ -1,31 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+int main()
+{
     int t;
     cin >> t;
     while (t--)
     {
         /* code */
-        int row, col;
+        long long row, col;
         cin >> row >> col;
-        int a, b;
-        int maxi = max(row, col);
+        long long a, b;
+        long long maxi = max(row, col);
 
-        if(maxi % 2){
+        if (maxi % 2)
+        {
             a = 1;
             b = maxi;
         }
-        else{
+        else
+        {
             a = maxi;
             b = 1;
         }
 
-        int ans = (maxi * maxi) - (abs(a - row) + abs(b - col));
+        long ans = (maxi * maxi) - (abs(a - row) + abs(b - col));
         cout << ans;
     }
-    
-
 
     return 0;
 }
