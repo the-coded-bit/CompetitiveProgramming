9172a0b420e6777ff850c4022fd694d66fe4b445
Arpit Gupta <theburningdesiresince2002@gmail.com>
2021-07-31 13:52:43 +0530

    updated goldmine code with all solutions recursive + memoization, tabulation

--- GoldMine.java
+++ GoldMine.java
@@ -10,32 +10,75 @@ public class GoldMine {
         int[][] arr = new int[n][m];
         
         for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++)
+            for(int j = 0; j < m; j++){
                 arr[i][j] = sc.nextInt();
-        }
-        sc.close();
-        int[][] dp = new int[n][m];
-        
-        for(int j = m - 1; j >= 0; j--){
-            for(int i = n- 1; i >= 0; i--) {
-                if(j == m - 1)
-                    dp[i][j] = arr[i][j];
-                else if(i == n - 1){
-                    dp[i][j] = Math.max(dp[i][j + 1], dp[i-1][j + 1]) + arr[i][j];
-                }
-                else if(i == 0){
-                    dp[i][j] = Math.max(dp[i][j + 1], dp[i + 1][j + 1]) + arr[i][j]; 
-                }
-                else{
-                    dp[i][j] = Math.max(dp[i][j + 1], Math.max(dp[i + 1][j + 1], dp[i - 1][j + 1])) + arr[i][j];
-                }
             }
         }
-     int maxi = 0;   
-     for(int i = 0; i < n; i++){
-         maxi = Math.max(dp[i][0], maxi);
-     } 
-     System.out.println(maxi);
+        sc.close();
+          
+        int[][] dp = new int[n][m];
+        
+        // int maxi = 0;
+        // for(int i = 0; i < n; i++){
+        //     for(int j = 0; j < m; j++){
+        //         maxi = Math.max(maxGold(arr, i, j, dp), maxi);
+        //     }
+        // }
+        
+        // System.out.println(maxi);
+        System.out.println(maxGoldTab(arr, n, m, dp));
+        
+        // for(int i = 0; i < n; i++){
+        //     for(int j = 0; j < m; j++)
+        //         System.out.print(dp[i][j] +" ");
+        //     System.out.println();    
+        // }
+        
+        
     }
-
+    
+    //recursive + memoization solution for Goldmine
+    public static int maxGold(int[][] arr, int r, int c, int[][] dp){
+        
+        //base condition
+        if(c == arr[0].length || r < 0 || r == arr.length)
+            return 0;
+        
+        
+        int up_right = maxGold(arr, r - 1, c + 1, dp);
+        int right = maxGold(arr, r, c + 1, dp);
+        int d_right = maxGold(arr, r + 1, c + 1, dp);
+        
+        if(dp[r][c] != 0)
+            return dp[r][c];
+        
+        int ans = Math.max(up_right, Math.max(right, d_right)) + arr[r][c];
+        return dp[r][c] = ans;
+    }
+    
+    
+    
+    //tabulation solution for Goldmine
+    public static int maxGoldTab(int[][] arr, int n, int m, int[][] dp){
+        
+        for(int c = m - 1; c >= 0; c--){
+            for(int r = n - 1; r >= 0; r--){
+                if(c == m - 1)
+                    dp[r][c] = arr[r][c];
+                else if(r == 0)
+                    dp[r][c] = Math.max(dp[r][c + 1], dp[r + 1][c + 1]) + arr[r][c];
+                else if(r == n - 1)
+                    dp[r][c] = Math.max(dp[r][c + 1], dp[r - 1][c + 1]) + arr[r][c];
+                else
+                    dp[r][c] = Math.max(dp[r + 1][c + 1], Math.max(dp[r][c + 1], dp[r - 1][c + 1])) + arr[r][c];
+            }
+        }
+        
+        
+        int maxi = 0;
+        for(int i = 0; i < n; i++)
+            maxi = Math.max(dp[i][0], maxi);
+            
+        return maxi;    
+    }
 }
