43fa606dab1c7b9069c7d251f9b276f0715407f9
Arpit Gupta <theburningdesiresince2002@gmail.com>
2021-11-22 19:00:04 +0530

    smallest pattern stacks

---
+++ smallestPattern.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+public class smallestPattern{
+  
+
+public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    String str = br.readLine();
+
+    // code
+    Stack< Integer> st = new Stack< >();
+
+    int num = 1;
+    for (int i = 0; i < str.length(); i++) {
+      char ch = str.charAt(i);
+      if (ch == 'd') { // when we encounter d
+        st.push(num);
+        num++;
+      } else { // when we encounter i
+        st.push(num);
+        num++;
+        while (st.size() > 0) {
+          System.out.print(st.pop());
+        }
+      }
+    }
+    st.push(num); // for last number
+    while (st.size() > 0) {
+      System.out.print(st.pop());
+    }
+ }
+}
