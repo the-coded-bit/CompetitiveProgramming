a6133b39fc01c865540dee2faa1433e05041d521
Arpit Gupta <theburningdesiresince2002@gmail.com>
2021-12-10 23:57:36 +0530

    basic operations update 1

--- BasicOperation.java
+++ BasicOperation.java
@@ -1,6 +1,7 @@
 import java.io.*;
 import java.util.*;
 
+
 //size of generic tree
 public class BasicOperation {
   private static class Node {
@@ -65,6 +66,115 @@ public class BasicOperation {
     return maxi;
   }
 
+  public static int height(Node node){
+
+    //maxi initialize to -1 has significance since it handles the base case when there is only one node in tree
+    int maxi = -1;
+    for(Node iNode : node.children){
+      maxi = Math.max(height(iNode), maxi);
+    }
+    int ans = maxi + 1;
+    return ans;
+  }
+
+  public static void traversals(Node node){
+    // write your code here
+    
+    //pre-order traversal
+    System.out.println("Node Pre " + node.data);
+    
+    //in-order traversal
+    for(Node iNode : node.children){
+        System.out.println("Edge Pre " + node.data + "--" + iNode.data);
+        traversals(iNode);
+        System.out.println("Edge Post " + node.data + "--" + iNode.data);
+    }
+    
+    //post-order traversal
+    System.out.println("Node Post " + node.data);
+  }
+
+  public static void levelOrderLinewise(Node node){
+    Queue<Node> mainQ = new ArrayDeque<>();
+    Queue<Node> childQ = new ArrayDeque<>();
+
+    mainQ.add(node);
+    while(mainQ.size() > 0){
+
+      node = mainQ.remove();
+      System.out.print(node.data + " ");
+
+      for(Node iNode : node.children){
+        childQ.add(iNode);
+      }
+
+      if(mainQ.size() == 0){
+        mainQ = childQ;
+        childQ = new ArrayDeque<>();
+        System.out.println();
+      }
+
+    }
+  }
+
+  public static void levelOrderLinewiseZZ(Node node){
+    // write your code here
+    Stack<Node> mainS = new Stack<>();
+    Stack<Node> childS = new Stack<>();
+    int level = 1;
+    
+    mainS.push(node);
+    
+    while(mainS.size() > 0){
+        
+        // remove
+        node = mainS.pop();
+        //print
+        System.out.print(node.data + " ");
+        //add according to level if level is even add in reverse order else in front order
+        if(level%2 == 1){
+            for(Node iNode : node.children){
+                childS.push(iNode);
+            }
+        }
+        else{
+            for(int i = node.children.size()-1; i >= 0; i--){
+                childS.push(node.children.get(i));
+            }
+        }
+        
+        if(mainS.size()== 0){
+            mainS = childS;
+            childS = new Stack<>();
+            System.out.println();
+            level++;
+        }
+    }
+  }
+
+  public static void mirror(Node node){
+
+    for(Node iNode : node.children){
+          mirror(iNode);
+    }
+    Collections.reverse(node.children);
+  }
+
+  public static boolean find(Node node, int data) {
+
+    //base case
+    if(node.data == data)
+        return true;
+        
+    for(Node iNode : node.children){
+        boolean ans = find(iNode, data);
+        if(ans == true)
+          return true;
+    }
+    
+    return false;    
+  }
+
   public static void main(String[] args) throws Exception {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     int n = Integer.parseInt(br.readLine());
@@ -80,6 +190,10 @@ public class BasicOperation {
     int m = max(root);
     System.out.println(m);
     display(root);
+    levelOrderLinewise(root);
+    levelOrderLinewiseZZ(root);
+    mirror(root);
+    
   }
 
 }
