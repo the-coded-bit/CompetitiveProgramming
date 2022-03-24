7ed12f69ad66cf2f6bb52501433e1b3735add81e
Arpit Gupta <theburningdesiresince2002@gmail.com>
2022-03-24 07:16:10 +0530

    all repeating except one question

---
+++ allrepeatingexceptone.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class allrepeatingexceptone {
+    
+  public static void main(String[] args){
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+    int[] arr = new int[n];
+    for(int i = 0 ; i < n; i++){
+      arr[i] = scn.nextInt();
+    }
+    scn.close();
+    //write your code here
+    int ans = 0;
+    for(int i = 0; i < n; i++){
+        ans = ans ^ arr[i];
+    }
+    System.out.println(ans);
+  }
+
+}
